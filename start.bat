2d414f95fcbdc584b2085b5d0336e88dc0c641ef
Lucas Morais <82246311+404six@users.noreply.github.com>
2022-09-20 09:52:45 -0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,4 +2,4 @@
 @set "pasta_geral=C:\projects\first"
 @set "pasta_scripts=C:\projects\first\vm\Scripts"
 
-cmd /k "cd /d %pasta_scripts% & activate & cd /d %pasta_geral% & py manage.py migrate & py manage.py runserver"
+cmd /k "cd /d %pasta_scripts% & activate & cd /d %pasta_geral% & py manage.py makemigrations & py manage.py migrate & py manage.py runserver"
